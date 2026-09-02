83c144c2dd7d1a2e2d23db4180780a88b018e886
Aiba <123001499+Aibally@users.noreply.github.com>
2026-09-02 15:27:33 +0200

    Add: new-21-game - submission-9

---
+++ Data Structures & Algorithms/new-21-game/submission-9.py
@@ -0,0 +1,17 @@
+class Solution:
+    def new21Game(self, n: int, k: int, maxPts: int) -> float:
+        if k==0 or n >= k+maxPts: return 1.0
+
+        dp = [0]*(n+1)
+        dp[0] = 1.0
+        res=0
+
+        for i in range(1,n+1):
+            start = max(0,i-maxPts)
+            end = min(i,k)
+
+            if start<end:
+                dp[i] = sum(dp[start:end]) / maxPts
+            if i >= k:
+                res += dp[i]
+        return res
