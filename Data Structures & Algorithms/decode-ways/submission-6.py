c9e188f4d9ef6f8edfba99c21967f25d8b2b961f
Aiba <123001499+Aibally@users.noreply.github.com>
2026-09-03 16:32:58 +0200

    Add: decode-ways - submission-6

---
+++ Data Structures & Algorithms/decode-ways/submission-6.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        memo={}
+        def recursion(i):
+            if i==len(s):return 1
+            if s[i] == "0": return 0
+            if i in memo: return memo[i]
+            
+            res = recursion(i+1)
+            
+            if i+1<len(s) and int(s[i:i+2]) <= 26:
+                res += recursion(i+2)
+            
+            memo[i]=res
+            return res
+        return recursion(0)
